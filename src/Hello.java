75d4e7491fdddebfbc99e8b0864d4ebd6a1e21a9
kkr <kirankr.92@gmail.com>
2021-12-30 18:30:34 +0530

    added one more java file

--- src/Hello.java
+++ src/Hello.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World") ;
 
-                int [] arr_marks = new int[6];
+        int [] arr_marks = new int[6];
         arr_marks[0]=97 ;
         arr_marks[1]=25;
         arr_marks[2]=35;
@@ -13,16 +13,16 @@ public class HelloWorld {
         arr_marks[4]=60 ;
         arr_marks[5]=84 ;
 
-            int i ;
-              for (i=0;i<6;i++)
-                  System.out.println("rollno:" + i + "scored" + evalauteClass(arr_marks[i]));
-              System.out.println("new");
+        int i ;
+        for (i=0;i<6;i++)
+            System.out.println("rollno:" + i + "scored" + evalauteClass(arr_marks[i]));
+        System.out.println("new");
 
     }
-     public static
+    public static
     String evalauteClass(int k)
     {
-       //fdskfj
+        //fdskfj
         if (k <35 )
         {
             return "FAIL" ;
@@ -33,8 +33,8 @@ public class HelloWorld {
             return "Second class" ;
         }
         else if (k>=60 && k<85 )
-         {
-        return "First class" ;
+        {
+            return "First class" ;
         }
         else
         {
